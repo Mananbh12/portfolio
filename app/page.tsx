8293a473c5a5bb8f287dcaaba7b403cb20ea2351
Arya Putra <mananbhrdwj@gmail.com>
2025-02-08 17:19:55 +0100

    portfolio fini, contenu a modifier juste

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,21 @@ import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex-justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+    <main className="relative bg-black-100 flex-justify-center items-center flex-col mx-auto sm:px-10 overflow-clip">
       <div>
         <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
